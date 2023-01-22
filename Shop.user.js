09c3bae4d5deba5adacb04d4811519f7f998de1b
T0HBA <121461213+T0HBA@users.noreply.github.com>
2023-01-22 10:41:57 -0800

    Add files via upload

---
+++ Shop.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         Tanki Online Store Opener
+// @version      0.1
+// @description  A hands off tool to keep the store opened in the tanki online test servers
+// @author       0xE3
+// @match        https://*.test-eu.tankionline.com/*
+// @icon         https://www.google.com/s2/favicons?domain=tankionline.com
+// @grant        none
+// ==/UserScript==
+
+const root = document.getElementById('root');
+
+const startupLoop = () => {
+  try {
+    init(root._reactRootContainer._internalRoot.current.memoizedState.element.type.prototype.store)
+  } catch (e) {
+    requestAnimationFrame(() => startupLoop());
+  }
+}
+
+startupLoop();
+
+const init = (store) => {
+  setInterval(() => {
+    store.state.shop.enabled = true;
+  }, 100);
+}
